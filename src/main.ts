6e644935c5647657199593e33d70010c85eae11f
Valery Shevchenko <ar4er.valera@gmail.com>
2023-02-14 14:53:27 +0300

    fix: webpack crutch

--- src/main.ts
+++ src/main.ts
@@ -5,43 +5,7 @@ import { AppModule } from './app.module';
 
 const port = process.env.port || 3000;
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { resolve } = require('path');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { readdir } = require('fs').promises;
-
-async function* getFiles(dir) {
-  const dirs = await readdir(dir, { withFileTypes: true });
-  for (const dirent of dirs) {
-    const res = resolve(dir, dirent.name);
-    if (dirent.isDirectory()) {
-      yield* getFiles(res);
-    } else {
-      yield res;
-    }
-  }
-}
-
-const dirPath = '~/../public/src/blocks';
-const jsFilePath = '~/../public/src/imports/common_imports.js';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('fs').writeFileSync(jsFilePath, '');
-
-async function packJsFiles() {
-  for await (const f of getFiles(dirPath)) {
-    if (f.endsWith('.js')) {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const data = require('fs').readFileSync(f);
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      require('fs').appendFileSync(jsFilePath, data + '\n');
-    }
-  }
-}
-
 async function bootstrap() {
-  await packJsFiles();
-
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
