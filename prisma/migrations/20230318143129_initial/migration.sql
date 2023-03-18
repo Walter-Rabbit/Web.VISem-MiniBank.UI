9c628851fdf55937c94ccfa605761a5bcdd1def3
Valery Shevchenko <ar4er.valera@gmail.com>
2023-03-18 17:42:09 +0300

    feat: orm

---
+++ prisma/migrations/20230318143129_initial/migration.sql
@@ -0,0 +1,123 @@
+-- CreateTable
+CREATE TABLE "UserProfile" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "birthDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "senderAccountId" INTEGER NOT NULL,
+    "receiverAccountId" INTEGER NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AccountDescription" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "duration" TIMESTAMP(3) NOT NULL,
+    "limit" DOUBLE PRECISION NOT NULL,
+    "monthPercent" DOUBLE PRECISION NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "AccountDescription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CreditDescription" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "duration" TIMESTAMP(3) NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "percent" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "CreditDescription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DepositDescription" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "duration" TIMESTAMP(3) NOT NULL,
+    "limit" DOUBLE PRECISION NOT NULL,
+    "yearPercent" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "DepositDescription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" SERIAL NOT NULL,
+    "descriptionId" INTEGER NOT NULL,
+    "ownerProfileId" INTEGER NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "isActive" BOOLEAN NOT NULL,
+    "serviceEndDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Credit" (
+    "id" SERIAL NOT NULL,
+    "descriptionId" INTEGER NOT NULL,
+    "ownerProfileId" INTEGER NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "moneyLeftToPay" DOUBLE PRECISION NOT NULL,
+    "serviceEndDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Credit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Deposit" (
+    "id" SERIAL NOT NULL,
+    "descriptionId" INTEGER NOT NULL,
+    "ownerProfileId" INTEGER NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "moneyPaid" DOUBLE PRECISION NOT NULL,
+    "serviceEndDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Deposit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserProfile_email_key" ON "UserProfile"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AccountDescription_name_key" ON "AccountDescription"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CreditDescription_name_key" ON "CreditDescription"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DepositDescription_name_key" ON "DepositDescription"("name");
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_senderAccountId_fkey" FOREIGN KEY ("senderAccountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_descriptionId_fkey" FOREIGN KEY ("descriptionId") REFERENCES "AccountDescription"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_ownerProfileId_fkey" FOREIGN KEY ("ownerProfileId") REFERENCES "UserProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Credit" ADD CONSTRAINT "Credit_descriptionId_fkey" FOREIGN KEY ("descriptionId") REFERENCES "CreditDescription"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Credit" ADD CONSTRAINT "Credit_ownerProfileId_fkey" FOREIGN KEY ("ownerProfileId") REFERENCES "UserProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Deposit" ADD CONSTRAINT "Deposit_descriptionId_fkey" FOREIGN KEY ("descriptionId") REFERENCES "DepositDescription"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Deposit" ADD CONSTRAINT "Deposit_ownerProfileId_fkey" FOREIGN KEY ("ownerProfileId") REFERENCES "UserProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
